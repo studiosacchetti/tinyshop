fb13d5be5d616f5cd8073c1282ad2ea1ad7baf6e
studiosacchetti <33520105+studiosacchetti@users.noreply.github.com>
2020-05-27 15:31:25 +0200

    Update addtowishlist.php

--- addtowishlist.php
+++ addtowishlist.php
@@ -4,19 +4,28 @@ ob_start();
 session_start();
 require_once 'config/connect.php';
 $uid = $_SESSION['customerid'];
+
 if (!isset($_SESSION['customer']) & empty($_SESSION['customer'])) {
     header('location: login.php');
 }
+
 if (isset($_GET['id']) & !empty($_GET['id'])) {
     $pid = $_GET['id'];
-    echo $sql = "INSERT INTO wishlist (pid, uid) VALUES ($pid, $uid)";
+    $sql = "SELECT * FROM wishlist WHERE pid = $pid AND uid =$uid";
     $res = mysqli_query($connection, $sql);
-    if ($res) {
-            // header('location: wishlist.php');
-            header("location: " . $_SERVER["HTTP_REFERER"]);    // stay in page while adding products into wishlist
+
+    if (mysqli_num_rows($res) == 0) {
+        $isql = "INSERT INTO wishlist (pid, uid) VALUES ($pid, $uid)";
+        $ires = mysqli_query($connection, $isql);
+
+        /* if ($ires) {
+          @todo toast notification
+          
+          header('location: wishlist.php');
+        } */
     }
-} else {
-        // header('location: wishlist.php');
-        header("location: " . $_SERVER["HTTP_REFERER"]);
 }
+
+// header('location: wishlist.php');
+header("location: " . $_SERVER["HTTP_REFERER"]);
 ?>
